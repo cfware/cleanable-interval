abe0e9e5f304ce4e3d0952faff7452a1637929dc
Corey Farrell <git@cfware.com>
2020-11-06 10:18:34 -0500

    fix: Reduce size

--- cleanable-interval.js
+++ cleanable-interval.js
@@ -1,5 +1,5 @@
-export default function cleanableInterval(fn, delay, ...args) {
+export default (fn, delay, ...args) => {
 	const handle = setInterval(fn, delay, ...args);
 
 	return () => clearInterval(handle);
-}
+};
